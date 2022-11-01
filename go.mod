affac500e1a8f5070f406ecc9cea22b6caf5136f
laurence <laurence.jones@live.co.uk>
2022-11-01 14:04:36 +0000

    File plugin with custom formatter

--- go.mod
+++ go.mod
@@ -13,11 +13,14 @@ require (
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mitchellh/go-testing-interface v1.0.0 // indirect
 	github.com/oklog/run v1.0.0 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	github.com/t-tomalak/logrus-easy-formatter v0.0.0-20190827215021-c074f06c5816
 	golang.org/x/net v0.0.0-20220418201149-a630d4f3e7a2 // indirect
-	golang.org/x/sys v0.0.0-20220513210249-45d2b4557a2a // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20220414192740-2d67ff6cf2b4 // indirect
 	google.golang.org/grpc v1.45.0 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 	"github.com/crowdsecurity/crowdsec/pkg/protobufs"
 	"github.com/hashicorp/go-hclog"
 	plugin "github.com/hashicorp/go-plugin"
+	log "github.com/sirupsen/logrus"
+	easy "github.com/t-tomalak/logrus-easy-formatter"
+	"gopkg.in/natefinch/lumberjack.v2"
 	"gopkg.in/yaml.v2"
 )
 
@@ -19,10 +22,26 @@ var logger hclog.Logger = hclog.New(&hclog.LoggerOptions{
 })
 
 type PluginConfig struct {
-	Name     string  `yaml:"name"`
-	LogLevel *string `yaml:"log_level"`
+	Name         string     `yaml:"name"`
+	LogLevel     *string    `yaml:"log_level"`
+	LogPath      string     `yaml:"log_path"`
+	LogFormatter string     `yaml:"log_formatter"`
+	Rotate       FileRotate `yaml:"rotate"`
+	LogFormat    LogFormat  `yaml:"log_format"`
+}
 
-	LogPath string `yaml:"log_path"`
+type FileRotate struct {
+	Enabled  bool  `yaml:"enabled"`
+	MaxSize  int   `yaml:"max_size"`
+	MaxFiles int   `yaml:"max_files"`
+	MaxAge   int   `yaml:"max_age"`
+	Compress *bool `yaml:"compress"`
+}
+
+type LogFormat struct {
+	CustomFormat     string `yaml:"custom_format"`
+	FormatterName    string `yaml:"formatter_name"`
+	CustomTimeFormat string `yaml:"custom_time_format"`
 }
 
 type FilePlugin struct {
@@ -30,8 +49,13 @@ type FilePlugin struct {
 }
 
 func (n *FilePlugin) Configure(ctx context.Context, config *protobufs.Config) (*protobufs.Empty, error) {
+	logger.Info("Configured called")
 	d := PluginConfig{}
 	if err := yaml.Unmarshal(config.Config, &d); err != nil {
+		return nil, err
+	}
+	if err := d.SetDefaultLoggerConfig(); err != nil {
+		logger.Info(fmt.Sprintf("Error happened %s", err.Error()))
 		return nil, err
 	}
 	n.ConfigByName[d.Name] = d
@@ -48,17 +72,7 @@ func (n *FilePlugin) Notify(ctx context.Context, notification *protobufs.Notific
 	} else {
 		logger.SetLevel(hclog.Info)
 	}
-
-	f, err := os.OpenFile(cfg.LogPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-
-	if err != nil {
-		return nil, err
-	}
-	// defer f.Close()
-
-	if _, err := f.WriteString(fmt.Sprintf("[%s] %s\n", notification.Name, notification.Text)); err != nil {
-		return nil, err
-	}
+	log.Info(notification.Text)
 
 	logger.Info(fmt.Sprintf("Appended new alert: [%s] %s\n", notification.Name, notification.Text))
 
@@ -71,7 +85,7 @@ func main() {
 		MagicCookieKey:   "CROWDSEC_PLUGIN_KEY",
 		MagicCookieValue: os.Getenv("CROWDSEC_PLUGIN_KEY"),
 	}
-
+	logger.Info("Plugin called")
 	plugin.Serve(&plugin.ServeConfig{
 		HandshakeConfig: handshake,
 		Plugins: map[string]plugin.Plugin{
@@ -82,4 +96,54 @@ func main() {
 		GRPCServer: plugin.DefaultGRPCServer,
 		Logger:     logger,
 	})
+}
+
+func (n *PluginConfig) SetDefaultLoggerConfig() error {
+
+	/*Configure logs*/
+	_maxsize := 500
+	if n.Rotate.MaxSize != 0 {
+		_maxsize = n.Rotate.MaxSize
+	}
+	_maxfiles := 3
+	if n.Rotate.MaxFiles != 0 {
+		_maxfiles = n.Rotate.MaxFiles
+	}
+	_maxage := 28
+	if n.Rotate.MaxAge != 0 {
+		_maxage = n.Rotate.MaxAge
+	}
+	_compress := true
+	if n.Rotate.Compress != nil {
+		_compress = *n.Rotate.Compress
+	}
+	// check if file exists
+	_, err := os.Stat(n.LogPath)
+	// create file if not exists, purposefully ignore errors
+	if os.IsNotExist(err) {
+		file, _ := os.OpenFile(n.LogPath, os.O_RDWR|os.O_CREATE, 0644)
+		file.Close()
+	}
+
+	LogOutput := &lumberjack.Logger{
+		Filename: n.LogPath,
+	}
+	if n.Rotate.Enabled {
+		LogOutput.MaxSize = _maxsize
+		LogOutput.MaxSize = _maxfiles
+		LogOutput.MaxSize = _maxage
+		LogOutput.Compress = _compress
+	}
+	log.SetOutput(LogOutput)
+	log.SetLevel(log.InfoLevel)
+	if n.LogFormat.CustomFormat != "" {
+		log.SetFormatter(&easy.Formatter{
+			TimestampFormat: "2006-01-02 15:04:05",
+			LogFormat:       n.LogFormat.CustomFormat,
+		})
+	} else {
+		logFormatter := &log.TextFormatter{TimestampFormat: n.LogFormat.CustomTimeFormat, FullTimestamp: true}
+		log.SetFormatter(logFormatter)
+	}
+	return nil
 }
